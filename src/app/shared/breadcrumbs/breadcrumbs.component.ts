97661ca0bbae0c69cc17685e0e9694d0f459727e
Alex Giuliano <u920722@proveedor.teco.com.ar>
2022-09-25 00:19:37 -0300

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BreadcrumbsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BreadcrumbsComponent implements OnDestroy {
+  public titulo: string = '';
+  public tituloSubs$: Subscription;
+  constructor(private _route: Router) {
+    this.tituloSubs$ = this.argumentosRuta().subscribe(({ titulo }) => {
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
+  argumentosRuta() {
+    return this._route.events.pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
+  }
 }
