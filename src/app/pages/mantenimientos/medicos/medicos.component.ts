3957da9d32cb9b1ef840d8b4bad8dd1afe156f2e
AlexGiuliano20 <alex.giuliano37@gmail.com>
2023-01-27 18:48:12 -0300

    CRUD Medicos

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Medico } from 'src/app/models/medico.model';
+
+import { MedicoService } from 'src/app/services/medico.service';
+import { ModalImagenService } from 'src/app/services/modal-imagen.service';
+
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MedicosComponent implements OnInit {
+  public cargando: boolean = true;
+  public medicos: Medico[] = [];
 
-  constructor() { }
+  constructor(
+    private _medicoService: MedicoService,
+    private _modalImagenService: ModalImagenService
+  ) {}
 
   ngOnInit(): void {
+    this.cargarMedicos();
   }
 
+  cargarMedicos() {
+    this.cargando = true;
+    this._medicoService.cargarMedicos().subscribe({
+      next: (medicos) => {
+        this.cargando = false;
+        this.medicos = medicos;
+      },
+    });
+  }
+
+  abrirModal(medico: Medico) {
+    this._modalImagenService.abrirModal(
+      'medicos',
+      medico._id || '',
+      medico.img
+    );
+  }
 }
--- src/app/services/medico.service.ts
+++ src/app/services/medico.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Medico } from '../models/medico.model';
+
+const base_url = environment.base_url;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MedicoService {
+  constructor(private _http: HttpClient) {}
 
-  constructor() { }
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers() {
+    return {
+      headers: { 'x-token': this.token },
+    };
+  }
+
+  cargarMedicos() {
+    const url: string = `${base_url}/medicos`;
+    return this._http
+      .get<Medico[]>(url, this.headers)
+      .pipe(map((res: any) => res.medicos));
+  }
+
+  crearMedico(medico: Medico) {
+    const url: string = `${base_url}/medicos`;
+    return this._http.post(url, { medico }, this.headers);
+  }
+
+  actualizarMedico(medico: Medico) {
+    const url: string = `${base_url}/medicos/${medico._id}`;
+    return this._http.put(url, medico, this.headers);
+  }
+
+  borrarMedico(_id: string) {
+    const url: string = `${base_url}/medicos/${_id}`;
+    return this._http.delete(url, this.headers);
+  }
 }
