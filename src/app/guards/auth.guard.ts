0b6170006b184c6d3e32147538b841b51091cd35
AlexGiuliano20 <alex.giuliano37@gmail.com>
2023-01-29 01:38:27 -0300

    LazyLoad

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,21 +2,36 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
 } from '@angular/router';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(
     private _usuarioService: UsuarioService,
     private _router: Router
   ) {}
+
+  canLoad(route: Route, segments: UrlSegment[]) {
+    return this._usuarioService
+      .validarToken()
+      .pipe(
+        tap(
+          (estaAutenticado) =>
+            !estaAutenticado && this._router.navigateByUrl('/login')
+        )
+      );
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this._usuarioService
